e7e3cc07a2aded455b34950b17ee08e076ed25ef
Kirill Volkovich <kirill.volkovich@opuscapita.com>
2018-03-04 23:44:41 +0300

    Update server-nodejs Docker build

--- packages/server-nodejs/docker-build.sh
+++ packages/server-nodejs/docker-build.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-docker build --no-cache -t opuscapita/filemanager:latest .
+VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+docker build \
+       --no-cache \
+       --build-arg VERSION=$VERSION \
+       --build-arg VCS_REF=`git rev-parse --short HEAD` \
+       --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+       -t ockvolkovich/filemanager-server:latest .

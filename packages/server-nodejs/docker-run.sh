44a88d6b88a8f6b5948c962b3a1c88340435fdc5
Kirill Volkovich <kirill.volkovich@opuscapita.com>
2018-03-05 16:41:09 +0300

    Success

--- packages/server-nodejs/docker-run.sh
+++ packages/server-nodejs/docker-run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -it -p 80:80 --mount type=bind,source="$(pwd)",target=/var/app-data ockvolkovich/filemanager-server
+docker run --rm -it -p 80:80 --mount type=bind,source="$(pwd)",target=/var/app-data ockvolkovich/filemanager-server:1.0.4
